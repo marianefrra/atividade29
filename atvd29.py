d061ad71e049acb0c405b8e4c52163eb8e89f6db
marianefrra] <marianerochaferreira48@gmail.com>
2024-10-01 19:57:23 -0300

    atividade29

---
+++ atvd29.py
@@ -0,0 +1,28 @@
+# Crie um programa que receba uma lista com os nomes dos alunos presentes em uma aula e exiba quantos alunos estão presentes e a lista dos nomes. Se o total de alunos presentes for menor que 5, exiba uma mensagem sugerindo uma revisão da lista de chamada.
+
+# Exemplo de entrada:
+# Alunos presentes: ['Ana', 'Bruno', 'Carlos', 'Daniela']
+
+# Exemplo de saída:
+# Alunos presentes: 4
+# Lista de alunos presentes: Ana, Bruno, Carlos, Daniela
+# Aviso: Poucos alunos presentes. Revisar lista de chamada.
+
+presentes = []
+alunos = []
+
+while True:
+    aluno = str(input("Digite os nomes dos alunos presentes (Digite feito parar acabar): "))
+    if aluno == 'feito':
+        break
+    presentes.append(aluno)
+    alunos.append(aluno)
+    
+    
+
+print("Alunos presentes: {}".format(len(presentes)))
+print(presentes)
+if len(presentes)<5:
+    print("Revise a lista de chamada")
+else:
+    print("Tudo certo")
